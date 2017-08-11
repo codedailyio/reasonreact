9c2a75a81c86472723321bea0c9d21220325ed59
Jason Brown <browniefed@gmail.com>
2017-08-11 12:16:15 -0700

    Add

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ReasonReactIndex extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default ReasonReactIndex;
